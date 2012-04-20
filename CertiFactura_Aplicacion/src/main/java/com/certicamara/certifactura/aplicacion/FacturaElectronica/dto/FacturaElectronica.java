87d97e030a97dfff469ca2717eb347a6bf868762
Steven <stevenrigby@seven4n.com>
2012-04-20 10:05:21 -0500

    Cambios a la factura electronica

--- CertiFactura_Aplicacion/src/main/java/com/certicamara/certifactura/aplicacion/FacturaElectronica/dto/FacturaElectronica.java
+++ CertiFactura_Aplicacion/src/main/java/com/certicamara/certifactura/aplicacion/FacturaElectronica/dto/FacturaElectronica.java
@@ -1,27 +1,74 @@
 package com.certicamara.certifactura.aplicacion.FacturaElectronica.dto;
 
+import java.util.Date;
+
 public class FacturaElectronica {
 
-	private String nit;
+private Integer idConsecutivo;
 	
-	private String nombre;
-
-	public FacturaElectronica(String nit, String nombre) {
+	private Date fecha;
+	
+	private Integer idEmisor;
+	
+	private Integer idReceptor;
+	
+	private String estado;
+	
+	public FacturaElectronica(Integer idConsecutivo, Date fecha,
+			Integer idEmisor, Integer idReceptor, String estado) {
 		super();
-		this.nit = nit;
-		this.nombre = nombre;
+		this.idConsecutivo = idConsecutivo;
+		this.fecha = fecha;
+		this.idEmisor = idEmisor;
+		this.idReceptor = idReceptor;
+		this.estado = estado;
 	}
 
-	public String getNit() {
-		return nit;
+	public Integer getIdConsecutivo() {
+		return idConsecutivo;
 	}
 
-	public String getNombre() {
-		return nombre;
+	public void setIdConsecutivo(Integer idConsecutivo) {
+		this.idConsecutivo = idConsecutivo;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Integer getIdEmisor() {
+		return idEmisor;
+	}
+
+	public void setIdEmisor(Integer idEmisor) {
+		this.idEmisor = idEmisor;
+	}
+
+	public Integer getIdReceptor() {
+		return idReceptor;
+	}
+
+	public void setIdReceptor(Integer idReceptor) {
+		this.idReceptor = idReceptor;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
 	}
 
 	@Override
 	public String toString() {
-		return "FacturaElectronica [nit=" + nit + ", nombre=" + nombre + "]";
+		return "FacturaElectronica [idConsecutivo=" + idConsecutivo
+				+ ", fecha=" + fecha + ", idEmisor=" + idEmisor
+				+ ", idReceptor=" + idReceptor + ", estado=" + estado + "]";
 	}
+	
 }
