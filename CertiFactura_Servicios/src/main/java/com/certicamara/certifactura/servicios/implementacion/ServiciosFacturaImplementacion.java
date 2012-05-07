2f99d2c709a54c1274b6d924005db2e545530b9f
jose <jose@Jose>
2012-05-07 17:37:52 -0500

    Ajuste en librerias al proyecto de servicios

--- CertiFactura_Servicios/src/main/java/com/certicamara/certifactura/servicios/implementacion/ServiciosFacturaImplementacion.java
+++ CertiFactura_Servicios/src/main/java/com/certicamara/certifactura/servicios/implementacion/ServiciosFacturaImplementacion.java
@@ -2,13 +2,16 @@ package com.certicamara.certifactura.servicios.implementacion;
 
 
 
+import java.rmi.RemoteException;
+
 import javax.jws.WebService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import com.certicamara.certifactura.aplicacion.Comando;
 import com.certicamara.certifactura.aplicacion.GestorComandos;
 import com.certicamara.certifactura.aplicacion.FacturaElectronica.ComandoCrearFacturaElectronica;
 import com.certicamara.certifactura.dominio.dtos.FacturaElectronicaDTO;
-import com.certicamara.certifactura.dto.ParejaDTO;
 import com.certicamara.certifactura.servicios.IServiciosFacturaElectronica;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -19,29 +22,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Seven4N Ltda.
  * Apr 27, 2012
  */
-@WebService(endpointInterface = "com.certicamara.certifactura.servicios.IServiciosFacturaElectronica")
+@WebService(endpointInterface = "com.certicamara.certifactura.servicios.IServiciosFacturaElectronica", targetNamespace = "http://www.certicamara.com/certifactura/ws")
 public class ServiciosFacturaImplementacion implements IServiciosFacturaElectronica
 {
 
 	@Autowired
-	GestorComandos gestorComandos;
-	
-	/* (non-Javadoc)
-	 * @see com.certicamara.certifactura.servicios.IserviciosFacturaElectronica#crearFacturaElectronica(java.lang.String, com.certicamara.certifactura.dominio.dtos.FacturaElectronicaDTO, java.util.Map)
-	 */
-	@Override
-	public void crearFacturaElectronica( String idEmisor, FacturaElectronicaDTO facturaCanonicaDTO,  ParejaDTO parejaFacturaParticular)
-	{
-		try{			
-			ObjectMapper mapper = new ObjectMapper();
-			String cadenaJsonFacturaCanonica = mapper.writeValueAsString( facturaCanonicaDTO );		
-			Comando comando = (Comando)new ComandoCrearFacturaElectronica(cadenaJsonFacturaCanonica, "Exito");			
-			gestorComandos.recibirComando(comando);
-		}catch( Exception ex ){
-			ex.printStackTrace( );
-		}
-		
-	}
+	GestorComandos gestorComandos;	
+
 
 	/* (non-Javadoc)
 	 * @see com.certicamara.certifactura.servicios.IserviciosFacturaElectronica#entregarFacturaElectronica(java.lang.String, java.lang.String)
@@ -52,5 +39,23 @@ public class ServiciosFacturaImplementacion implements IServiciosFacturaElectron
 		// TODO Auto-generated method stub
 		
 	}
+
+	/* (non-Javadoc)
+	 * @see com.certicamara.certifactura.servicios.IServiciosFacturaElectronica#crearFacturaElectronica(java.lang.String, com.certicamara.certifactura.servicios.FacturaElectronicaDTO, java.lang.Object)
+	 */
+	@Override
+	public void crearFacturaElectronica( String idEmisor, FacturaElectronicaDTO facturaElectronicaCanonica, Object camposAdicionales ) throws RemoteException
+	{
+		try{			
+			ObjectMapper mapper = new ObjectMapper();
+			String cadenaJsonFacturaCanonica = mapper.writeValueAsString( facturaElectronicaCanonica );		
+			Comando comando = (Comando)new ComandoCrearFacturaElectronica(cadenaJsonFacturaCanonica, "Exito");			
+			gestorComandos.recibirComando(comando);
+		}catch( Exception ex ){
+			ex.printStackTrace( );
+		}
+		
+	}
+
 
 }
