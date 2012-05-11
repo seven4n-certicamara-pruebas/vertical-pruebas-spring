654e689254c052507dbd73610246d71f46d61c75
Marcial Moreno <marcialmoreno@seven4n.com>
2012-05-11 17:10:11 -0500

    Actualización del ant, y nueva Clase con Constantes de BD

---
+++ CertiFactura_Infraestructura/src/main/java/com/certicamara/certifactura/infraestructura/constantes/ConstantesPersistenciaCertiFactura.java
@@ -0,0 +1,18 @@
+package com.certicamara.certifactura.infraestructura.constantes;
+
+/**
+ * CertiFactura
+ * Certicámara S.A.
+ * ConstantesInyeccionCertiFactura
+ * @author Seven4N Ltda.
+ * Apr 25, 2012
+ */
+public class ConstantesPersistenciaCertiFactura
+{
+	public static final String NOMBRE_BASE_DATOS_CERTIFACTURA = "certifacturadb";
+}
+
+
+
+
+
