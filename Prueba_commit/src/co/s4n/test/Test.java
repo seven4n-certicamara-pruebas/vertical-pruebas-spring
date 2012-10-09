c5c1e62d5a57b1a8004ac0e978853dd130cacfcb
Steven <stevenrigby@seven4n.com>
2012-10-09 15:58:34 -0500

    Prueba en el master branch

--- Prueba_commit/src/co/s4n/test/Test.java
+++ Prueba_commit/src/co/s4n/test/Test.java
@@ -3,6 +3,8 @@ package co.s4n.test;
 public class Test {
 	
 	private String test;
+	
+	private String test10;
 
 
 	public String getTest() {
@@ -11,6 +13,22 @@ public class Test {
 
 	public void setTest(String test) {
 		this.test = test;
+	}
+
+	/**
+	 * @return the test10
+	 */
+	public String getTest10( )
+	{
+		return test10;
+	}
+
+	/**
+	 * @param test10 the test10 to set
+	 */
+	public void setTest10( String test10 )
+	{
+		this.test10 = test10;
 	}
 
 }
