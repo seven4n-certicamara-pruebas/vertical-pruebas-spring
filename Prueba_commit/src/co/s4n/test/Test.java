21f5458c3ac02035d00365dd110613655080d44e
Steven <stevenrigby@seven4n.com>
2012-10-09 15:48:56 -0500

    Prueba branch 1

--- Prueba_commit/src/co/s4n/test/Test.java
+++ Prueba_commit/src/co/s4n/test/Test.java
@@ -3,7 +3,25 @@ package co.s4n.test;
 public class Test {
 	
 	private String test;
+	
+	private String test2;
 
+
+	/**
+	 * @return the test2
+	 */
+	public String getTest2( )
+	{
+		return test2;
+	}
+
+	/**
+	 * @param test2 the test2 to set
+	 */
+	public void setTest2( String test2 )
+	{
+		this.test2 = test2;
+	}
 
 	public String getTest() {
 		return test;
