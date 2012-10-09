3b2a6e47b2fc1f67fb5b869412713eb41a3de8d8
Steven <stevenrigby@seven4n.com>
2012-10-09 16:24:08 -0500

    prueba en el branch

--- Prueba_commit/src/co/s4n/test/Test.java
+++ Prueba_commit/src/co/s4n/test/Test.java
@@ -5,27 +5,7 @@ public class Test {
 	private String test;
 	
 
-	private String test2;
-
-	private String test10;
-
-
-	private String test3;
-	/**
-	 * @return the test2
-	 */
-	public String getTest2( )
-	{
-		return test2;
-	}
-
-	/**
-	 * @param test2 the test2 to set
-	 */
-	public void setTest2( String test2 )
-	{
-		this.test2 = test2;
-	}
+	private String test6;
 
 	public String getTest() {
 		return test;
@@ -36,35 +16,20 @@ public class Test {
 	}
 
 	/**
-	 * @return the test3
+	 * @return the test6
 	 */
-	public String getTest3( )
+	public String getTest6( )
 	{
-		return test3;
+		return test6;
 	}
 
 	/**
-	 * @param test3 the test3 to set
+	 * @param test6 the test6 to set
 	 */
-	public void setTest3( String test3 )
+	public void setTest6( String test6 )
 	{
-		this.test3 = test3;
+		this.test6 = test6;
 	}
 
-	/**
-	 * @return the test10
-	 */
-	public String getTest10( )
-	{
-		return test10;
-	}
-
-	/**
-	 * @param test10 the test10 to set
-	 */
-	public void setTest10( String test10 )
-	{
-		this.test10 = test10;
-	}
 
 }
